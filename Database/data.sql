2e1df5a9fda6f5530d31e35cf38b82042ccf62d0
VInod Prajapati <150426114+Vinax846@users.noreply.github.com>
2026-04-27 02:34:05 +0530

    Add files via upload

---
+++ Database/data.sql
@@ -0,0 +1,7 @@
+create database employeemanagement;
+use employeemanagement;
+create table login(username varchar(20), password varchar(20));
+select * from login;
+insert into login values ('techcoder', '123456789')
+CREATE table employee (name varchar(40), fname varchar(40), dob varchar(40), salary varchar(40), address varchar(40), phone varchar(40), email varchar(40), education varchar(40), designation varchar(40), aadhar varchar(40), empID varchar(40));
+select * from employee;
